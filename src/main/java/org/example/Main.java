3d8270c5de4e47b3024850458a5ab2f1934d3837
Andrey Slepukhov <a668776@yandex.ru>
2023-03-28 16:41:09 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.List;
+
+public class Main {
+  public static void main(String[] args) {
+    final var validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+    int port = 9999;
+
+    Server server = new Server(port, validPaths);
+    server.run();
+
+  }
+}
+
+
